db2bbaba6a77028e1429f4fdf3ff4f871f79943a
Samvel Tomeyan <tomeyansam@gmail.com>
2026-07-14 12:20:53 +0300

    update

--- JS1/00-first.js
+++ JS1/00-first.js
@@ -56,6 +56,7 @@ console.log(!0); // true
 console.log(!""); //true
 console.log(!!0); //false
 console.log(!!""); //false
+
 //логическое ИЛИ
 console.log(false || false); //false
 console.log(false || true); //true
