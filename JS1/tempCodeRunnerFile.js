e121d2328fa770a6bfa8d07685ee7661890b6b07
Samvel Tomeyan <tomeyansam@gmail.com>
2026-07-17 14:51:53 +0300

    update

--- JS1/tempCodeRunnerFile.js
+++ JS1/tempCodeRunnerFile.js
@@ -1,6 +1,6 @@
-let fib = [0, 1];
-const ppp = 23;
+let fib = [1, 1];
+const ppp = 5;
 for (let i = 2; i < ppp; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
 }
-console.log(fib.slice(0, -1).join(' '));
+console.log(fib.join(' '));
