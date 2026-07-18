ba1c524e06f1b630057f0f27126429cb381d7bb7
Samvel Tomeyan <tomeyansam@gmail.com>
2026-07-18 23:27:15 +0300

    update a task

--- JS1/tempCodeRunnerFile.js
+++ JS1/tempCodeRunnerFile.js
@@ -5,14 +5,18 @@ function domainName(url) {
         let url2 = url.replace(part1, '');
         let part2 = url2.slice(url2.indexOf('.'));
 
-        return url2.replace(part2, '')
+        return url2.replace(part2, '');
 
-    } else {
-        let part1 = url.slice(0, url.indexOf('//') + 2)
+    } else if(url.includes('//')) {
+        let part1 = url.slice(0, url.indexOf('//') + 2);
 
         let url2 = url.replace(part1, '');
         let part2 = url2.slice(url2.indexOf('.'));
 
         return url2.replace(part2, '');
+    } else{
+        let part1 = url.slice(url.indexOf('.'));
+
+        return url.replace(part1, '');
     }
 }
