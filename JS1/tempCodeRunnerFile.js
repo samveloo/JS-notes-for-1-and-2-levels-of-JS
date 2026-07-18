831691a122015e0537649a366bbcbca057029f9d
Samvel Tomeyan <tomeyansam@gmail.com>
2026-07-18 23:13:47 +0300

    solve a task for js with 5kyu

--- JS1/tempCodeRunnerFile.js
+++ JS1/tempCodeRunnerFile.js
@@ -1,6 +1,18 @@
-let fib = [1, 1];
-const ppp = 5;
-for (let i = 2; i < ppp; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+function domainName(url) {
+    if (url.includes('www.')) {
+        let part1 = url.slice(0, url.indexOf('www.') + 4);
+
+        let url2 = url.replace(part1, '');
+        let part2 = url2.slice(url2.indexOf('.'));
+
+        return url2.replace(part2, '')
+
+    } else {
+        let part1 = url.slice(0, url.indexOf('//') + 2)
+
+        let url2 = url.replace(part1, '');
+        let part2 = url2.slice(url2.indexOf('.'));
+
+        return url2.replace(part2, '');
+    }
 }
-console.log(fib.join(' '));
