9791cbd81c7cbb5dbe8cd4841b60b8e5b02ea2be
Samvel Tomeyan <tomeyansam@gmail.com>
2026-08-09 11:42:45 +0300

    solve a task on codewars

--- JS1/tempCodeRunnerFile.js
+++ JS1/tempCodeRunnerFile.js
@@ -1,9 +1,14 @@
-function solution(number){
-  let total = 0;
-  
-  for(let i = 0; i < number; i++){
-    if(!(i % 3) || !(i % 5)) total += i;
-  }
-
-  return total;
+function likes(names) {
+  if (!(names.length)) return 'no one likes this';
+  else if (names.length === 1) return `${names[0]} likes this`;
+  else if (names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  else if (names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  else if (names.length === 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  else if (names.length > 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
+
+console.log(likes([]));
+console.log(likes(["Peter"]));
+console.log(likes(["Jacob", "Alex"]));
+console.log(likes(["Max", "John", "Mark"]));
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
